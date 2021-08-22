4e747fc818759c7131c9f0e84d20653fd93d6786
Carlosdc206 <89317743+Carlosdc206@users.noreply.github.com>
2021-08-21 22:22:18 -0500

    Add files via upload

---
+++ Sum #8.cpp
@@ -0,0 +1,16 @@
+
+int i, t, r;
+main(){
+		printf("introduzca la tabla:");
+		scanf("%d", &t);
+		for(i = 5; i <= 25; i <= 25; i++)
+	{
+			r = t*i;
+			printf("%d x %d = %d\n", t, i, r);
+		
+	}
+	getch();
+	
+	
+	
+}
